e50714edcad06dc8db7b957e1ecbf69e1debc6f2
Ibrahim Khaled Reguieg <khaled.reguieg@gmail.com>
2016-05-02 13:43:10 +0200

    fixed_point.h: - moved implementation of abs() friend function out of class - Fix:  Replaced hardcoded fractions with non-type template - Added implementation for types != 8, 16, 32 or 64
    fixed_point.test.cpp:
    - Refactoring

--- fixed_point.test.cpp
+++ fixed_point.test.cpp
@@ -39,9 +39,10 @@ auto operator<<(std::ostream &os, fixed_point<Decimal, Fraction> const &f) -> st
     return os;
 }
 
-int main() {
-    const fixed_point<16, 16> fp1(6.375f);
-    const fixed_point<16, 16> fp2(-4.0f);
+template<int8_t Decimal, int8_t Fraction>
+int test() {
+    const fixed_point<Decimal, Fraction> fp1(6.375f);
+    const fixed_point<Decimal, Fraction> fp2(-4.0f);
 
     // -------------------------------------------------------------------------
     EXPECT_EQ(6, int(fp1));
@@ -52,8 +53,8 @@ int main() {
 
     // -------------------------------------------------------------------------
     // comparison and ordering
-    assert(fp1 == (fixed_point<16, 16>(6.375)));
-    assert(-fp1 == (fixed_point<16, 16>(-6.375f)));
+    assert(fp1 == (fixed_point<Decimal, Fraction>(6.375)));
+    assert(-fp1 == (fixed_point<Decimal, Fraction>(-6.375f)));
     assert(fp2 != fp1);
     assert(fp2 < fp1);
     assert(fp1 > fp2);
@@ -62,50 +63,58 @@ int main() {
 
     // -------------------------------------------------------------------------
     // arithmetics
-    EXPECT_EQ((fixed_point<16, 16>(2.375f)), fp1 + fp2);
-    EXPECT_EQ((fixed_point<16, 16>(10.375f)), fp1 - fp2);
-    EXPECT_EQ((fixed_point<16, 16>(-25.5f)), fp1 * fp2);
-    EXPECT_EQ((fixed_point<16, 16>(-1.59375f)), fp1 / fp2);
-
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(2.375f)), fp1 + fp2);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(10.375f)), fp1 - fp2);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(-25.5f)), fp1 * fp2);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(-1.59375f)), fp1 / fp2);
     // -------------------------------------------------------------------------
     // arithmetics assignment
-    fixed_point<16, 16>
+    fixed_point<Decimal, Fraction>
             fp3 = fp1;
     fp3 += fp2;
-    EXPECT_EQ((fixed_point<16, 16>(2.375f)), fp3);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(2.375f)), fp3);
     fp3 = fp1;
     fp3 -= fp2;
-    EXPECT_EQ((fixed_point<16, 16>(10.375f)), fp3);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(10.375f)), fp3);
     fp3 = fp1;
     fp3 *= fp2;
-    EXPECT_EQ((fixed_point<16, 16>(-25.5f)), fp3);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(-25.5f)), fp3);
     fp3 = fp1;
     fp3 /= fp2;
-    EXPECT_EQ((fixed_point<16, 16>(-1.59375f)), fp3);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(-1.59375f)), fp3);
 
     // -------------------------------------------------------------------------
     // pre/post - increment/decrement
     fp3 = fp1;
-    EXPECT_EQ((fixed_point<16, 16>(7.375f)), ++fp3);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(7.375f)), ++fp3);
     fp3 = fp1;
-    EXPECT_EQ((fixed_point<16, 16>(5.375f)), --fp3);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(5.375f)), --fp3);
     fp3 = fp1;
-    EXPECT_EQ((fixed_point<16, 16>(6.375f)), fp3++);
-    EXPECT_EQ((fixed_point<16, 16>(7.375f)), fp3);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(6.375f)), fp3++);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(7.375f)), fp3);
     fp3 = fp1;
-    EXPECT_EQ((fixed_point<16, 16>(6.375f)), fp3--);
-    EXPECT_EQ((fixed_point<16, 16>(5.375f)), fp3);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(6.375f)), fp3--);
+    EXPECT_EQ((fixed_point<Decimal, Fraction>(5.375f)), fp3);
 
     // -------------------------------------------------------------------------
     // trigonometric functions
-    auto sinus1 = fixed_point<16, 16>(std::sin(0.5f));
-    auto sinus2 = sin((fixed_point<16, 16>) (0.5f));
+    auto sinus1 = fixed_point<Decimal, Fraction>(std::sin(0.5f));
+    auto sinus2 = sin((fixed_point<Decimal, Fraction>) (0.5f));
 
-    auto cosine1 = fixed_point<16, 16>(std::cos(0.5f));
-    auto cosine2 = cos((fixed_point<16, 16>) (0.5f));
+    auto cosine1 = fixed_point<Decimal, Fraction>(std::cos(0.5f));
+    auto cosine2 = cos((fixed_point<Decimal, Fraction>) (0.5f));
 
-    auto fpt = fixed_point<16, 16>(0.01f);
+    auto fpt = fixed_point<Decimal, Fraction>(0.01f);
 
     EXPECT_CLOSE(sinus1, sinus2, fpt);
     EXPECT_CLOSE(cosine1, cosine2, fpt);
+}
+
+int main() {
+    test<16, 16>();
+    test<8, 7>();
+    test<24, 8>();
+    //test<5, 3>();
+    std::cout << "Test ended." << std::endl;
+    return 0;
 }
